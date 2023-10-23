c848a2bd929c9a4b8b37d125de047644fa4620b8
João Victor <69174195+galvao845@users.noreply.github.com>
2023-10-23 14:33:32 -0300

    Ajustando teste

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 import Home from './code/Home.js';
 import NoData from './code/NoData.js';
